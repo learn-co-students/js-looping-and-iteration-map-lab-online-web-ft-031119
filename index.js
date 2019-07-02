21b4ec4e0d7ef3a9fcaab58c98ea1c5c4e1ad89c
darrendaz <darren.lou@gmail.com>
2019-07-02 16:24:04 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file.
+const driverList = [
+  'Bobby Smith',
+  'Sammy Watkins',
+  'Sally Jenkins',
+  'Annette Sawyer',
+  'Sarah Hucklebee',
+  'bobby ridge'
+];
+
+
+function lowerCaseDrivers(driverList) {
+  return driverList.map(driver => driver.toLowerCase())
+};
+
+function nameToAttributes(driverList) {
+  return driverList.map(driver => {
+    let firstLast = driver.split(" ");
+    return Object.assign({}, { firstName: firstLast[0], lastName: firstLast[1] })
+  });
+};
+
+function attributesToPhrase(driverList) {
+  return driverList.map(driver => `${driver.name} is from ${driver.hometown}`)
+};
